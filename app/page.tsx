dda79cee4ff1063dd26dbca44f252d51c3876545
jiraporn_c <chm.jiraporn@gmail.com>
2023-05-19 15:56:03 +0700

    initail liff

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,29 @@
+"use client"
+
 import Link from "next/link";
+import { useEffect } from "react";
 
 export default function Home() {
+  const liffId = process.env.NEXT_PUBLIC_LIFF_ID as string
+
+  const liffInitial = async () => {
+    const liff = (await import('@line/liff')).default
+    try {
+      await liff.init({ liffId });
+      console.log('initially loaded liff');
+    } catch (error: any) {
+      console.error('liff init error', error.message)
+    }
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    } else {
+      console.log('already logged in')
+    }
+  }
+  useEffect(() => { liffInitial() })
   return (
     <main>
-      <h1>Home Page</h1>
+      <h1>Home Page 2</h1>
       <p>
         <Link href="/users">
           Users
