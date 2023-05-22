83355c1eadc2a078e806e062fbf00681f5f52162
jiraporn_c <chm.jiraporn@gmail.com>
2023-05-22 14:08:04 +0700

    check open in line browser

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,30 @@ import { useEffect, useState } from "react";
 export default function Home() {
   const liffId = "1661146958-M2X7n4Bx"
   const [profile, setprofile] = useState<any>();
+  const [runningInLine, setrunningInLine] = useState<boolean>();
+
 
   const liffInitial = async () => {
-   const liff = (await import('@line/liff')).default
+    const liff = (await import('@line/liff')).default
     try {
       await liff.init({ liffId });
       console.log('initially loaded liff');
     } catch (error: any) {
       console.error('liff init error', error.message)
     }
-    if (!liff.isLoggedIn()) {
-      liff.login();
+    if (liff.isInClient()) {
+      setrunningInLine(true);
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        const profile = await liff.getProfile();
+        setprofile(profile);
+        console.log('already logged in')
+      }
     } else {
-      const profile = await liff.getProfile();
-      setprofile(profile);
-      console.log('already logged in')
+      setrunningInLine(false);
     }
+
   }
   useEffect(() => { liffInitial() }, [])
   return (
@@ -34,6 +42,8 @@ export default function Home() {
           Users
         </Link>
       </p>
+      {runningInLine ? <p>Running in Line</p>
+        : <p>Not running in Line</p>}
     </main>
   )
 }
